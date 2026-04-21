22760fb17a09cd0428135e4b46634bb77332e8b3
Harfdsfsq <tevez9595@gmail.com>
2026-04-21 18:21:26 +0300

    а

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
 from . import runtime_bot_fix  # noqa: F401
 from . import runtime_extra_hotfix  # noqa: F401
 from . import runtime_code_and_logs_fix  # noqa: F401
+from . import runtime_offer_source_fix  # noqa: F401
