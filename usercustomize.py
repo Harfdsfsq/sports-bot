10ba5ef98e4c362b3117776c5d6ae03cbf43a97c
HARIZON <tevez9595@gmail.com>
2026-05-04 01:37:40 +0300

    Install official SharpAPI endpoint patch

--- usercustomize.py
+++ usercustomize.py
@@ -5,3 +5,9 @@ try:
     sharpapi_runtime_patch.install()
 except Exception:
     pass
+
+try:
+    from app.providers import sharpapi_official_patch
+    sharpapi_official_patch.install()
+except Exception:
+    pass
